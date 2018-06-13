317469c02f268dffa49d7cb66289d1b647076ea9
Karthikeyan Kumar <33703136+kkumar1994@users.noreply.github.com>
2018-06-13 16:43:40 -0500

    Add files via upload
    Controlling Servo using Arduino

---
+++ ServoControl.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>               
+Servo SS;        //initialize a servo object for the connected servo  
+                
+int angle = 0;    
+ 
+void setup() 
+{ 
+  SS.attach(9);      // attach the signal pin of servo to pin9 of arduino
+} 
+  
+void loop() 
+{ 
+  for(angle = 0; angle < 180; angle += 1)  
+  {                                  
+    SS.write(angle);                 //command to rotate the servo to the specified angle
+    delay(15);                       
+  } 
+ 
+  delay(1000);
+  
+  for(angle = 180; angle>=1; angle-=5)      
+  {                                
+    SS.write(angle);              //command to rotate the servo to the specified angle
+    delay(5);                       
+  } 
+
+    delay(1000);
+}
